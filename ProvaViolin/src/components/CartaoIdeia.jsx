61924038ce187966275892866b1f6715feb4823f
benicioalvess <benicio.alves@estudante.ifms.edu.br>
2026-09-24 19:08:39 +0000

    Voltando para o commit Banco das Ideias

--- ProvaViolin/src/components/CartaoIdeia.jsx
+++ ProvaViolin/src/components/CartaoIdeia.jsx
@@ -1,12 +1,12 @@
 function CartaoIdeia({ ideia, aoAlternar, aoEditar, aoExcluir }) {
   return (
     <li>
-      <span className={ideia.completed ? 'executed' : ''}>
-        {ideia.title} ({ideia.completed ? 'executed' : 'quase indo'})
+      <span className={ideia.completed ? 'executada' : ''}>
+        {ideia.title} ({ideia.completed ? 'executada' : 'pendente'})
       </span>
       <span>
         <button onClick={() => aoAlternar(ideia)}>
-          {ideia.completed ? 'Reabrir' : ' executed'}
+          {ideia.completed ? 'Reabrir' : 'Marcar executada'}
         </button>
         <button onClick={() => aoEditar(ideia)}>Edits</button>
         <button onClick={() => aoExcluir(ideia.id)}>Exclói</button>
